f6bbfb106903edf10d5f70ed34d8b0f9a560184b
Klein Boonschate BJ <a.kleinboonschate@interactiveblueprints.nl>
2019-04-27 11:47:18 +0200

    Updated

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -31,4 +31,28 @@ class DefaultController extends AbstractController
             ]);
         }
     }
+
+    /**
+     * @param ApiManager $apiManager
+     * @param            $siteId
+     * @param string     $slug
+     * @return Response
+     * @Route("/example/{siteId}/{slug}", name="example")
+     */
+    public function example(ApiManager $apiManager, $siteId, $slug = 'home')
+    {
+        if(!$site = $apiManager->getSite($this->getParameter('api_end_point_example') . $siteId)) {
+            return new Response('Sorry, no site found', Response::HTTP_BAD_REQUEST);
+        } elseif(!$page = $apiManager->getPage($apiManager, $site, $slug)) {
+            return new Response('Sorry, no page found[1]', Response::HTTP_BAD_REQUEST);
+        } elseif(empty($page['route'])) {
+            return new Response('<h1>No page found</h1>[2]', Response::HTTP_NOT_FOUND);
+        } else {
+
+            return $this->render($site['template'] . '/' . $page['route'] . '/' . $page['type'] . '.html.twig', [
+                'site' => $site,
+                'page' => $page
+            ]);
+        }
+    }
 }
