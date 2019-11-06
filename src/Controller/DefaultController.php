64891fdacadbf4441af5f23350f744cca1cb3d8f
Klein Boonschate BJ <a.kleinboonschate@interactiveblueprints.nl>
2019-11-06 15:04:55 +0100

    Updated

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,30 +14,6 @@ class DefaultController extends AbstractController
 
     /**
      * @param ApiManager $apiManager
-     * @param            $siteId
-     * @param string     $slug
-     * @return Response
-     * @Route("/example/{siteId}/{slug}",
-     *     name="example")
-     */
-    public function example(ApiManager $apiManager, $siteId, $slug = 'home')
-    {
-        if (!$site = $apiManager->getSite($this->getParameter('api_end_point_example') . $siteId)) {
-            return new Response('Sorry, no site found[1]', Response::HTTP_BAD_REQUEST);
-        } elseif(!$page = $apiManager->getPage($apiManager, $site, $slug)) {
-            return new Response('Sorry, no page found[1]', Response::HTTP_BAD_REQUEST);
-        } elseif(empty($page['route'])) {
-            return new Response('Sorry, no page found[2]', Response::HTTP_NOT_FOUND);
-        } else {
-            return $this->render($site['template'] . '/' . $page['route'] . '/' . $page['type'] . '.html.twig', [
-                'site' => $site,
-                'page' => $page
-            ]);
-        }
-    }
-
-    /**
-     * @param ApiManager $apiManager
      * @return Response
      * @Route("/robots.{_format}",
      *     name="robots",
@@ -75,6 +51,30 @@ class DefaultController extends AbstractController
     }
 
     /**
+     * @param ApiManager $apiManager
+     * @param            $siteId
+     * @param string     $slug
+     * @return Response
+     * @Route("/example/{siteId}/{slug}",
+     *     name="example")
+     */
+    public function example(ApiManager $apiManager, $siteId, $slug = 'home')
+    {
+        if (!$site = $apiManager->getSite($this->getParameter('api_end_point_example') . $siteId)) {
+            return new Response('Sorry, no site found[1]', Response::HTTP_BAD_REQUEST);
+        } elseif(!$page = $apiManager->getPage($apiManager, $site, $slug)) {
+            return new Response('Sorry, no page found[1]', Response::HTTP_BAD_REQUEST);
+        } elseif(empty($page['route'])) {
+            return new Response('Sorry, no page found[2]', Response::HTTP_NOT_FOUND);
+        } else {
+            return $this->render($site['template'] . '/' . $page['route'] . '/' . $page['type'] . '.html.twig', [
+                'site' => $site,
+                'page' => $page
+            ]);
+        }
+    }
+
+    /**
      * @param ApiManager          $apiManager
      * @param TranslatorInterface $translator
      * @param Request             $request
@@ -97,7 +97,7 @@ class DefaultController extends AbstractController
 
             $search = $apiManager->getSearch($apiManager, $site, $request->get('needle'));
 
-            $page['page']['metaTitle'] = 'Zoeken:' . $request->get('needle');
+            $page['page']['metaTitle'] = $translator->trans('Zoeken') . ':' . $request->get('needle');
 
             return $this->render($site['template'] . '/pages/search-01.html.twig', [
                 'site' => $site,
